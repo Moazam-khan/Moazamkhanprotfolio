618d79d0300765a0a3932e9a5d09d057c266f163
Moazam-khan <moazamkhan8999@gmail.com>
2025-08-26 10:32:12 +0500

    imageoptimiozeradd

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    loader: 'custom',
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+  env: {
+    storePicturesInWEBP: 'true',
+    generateAndUseBlurImages: 'true',
+    imageFileExtensions: 'jpg,jpeg,png,webp,gif,ico,svg',
+    nextImageExportOptimizer_imageFolderPath: 'public/assets',
+    nextImageExportOptimizer_exportFolderPath: 'out',
+    nextImageExportOptimizer_quality: '75',
+    nextImageExportOptimizer_storePicturesInWEBP: 'true',
+    nextImageExportOptimizer_generateAndUseBlurImages: 'true',
+  },
 };
 
 export default nextConfig;
