2a1be9825eb2febb5cd7b104e039fed1ec2cd2bf
Engr Moazam khan <moazamkhan8999@gmail.com>
2026-01-03 17:48:57 +0300

    og

---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://moazamkhan.com'
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/', '/admin/', '/private/'],
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+}
